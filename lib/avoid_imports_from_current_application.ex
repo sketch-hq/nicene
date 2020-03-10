f305de0130b34ad66ac474e9cd95b922d2e8bd8a
Devon Estes <devon.c.estes@gmail.com>
2020-03-10 10:07:50 +0100

    Add new check to avoid imports from configured namespaces
    Having imports from modules defined in our own application can really
    slow down incremental compile times, and it would be nice to also be
    more explicit about where these functions are coming from.

---
+++ lib/avoid_imports_from_current_application.ex
@@ -0,0 +1,64 @@
+defmodule Nicene.AvoidImportsFromCurrentApplication do
+  @moduledoc """
+  Avoid importing functions from modules in the current OTP application, as this can really slow
+  down incremental compile times. Importing frunctions from dependent applications are fine since
+  those don't re-compile when making changes.
+  """
+
+  @explanation [check: @moduledoc]
+
+  use Credo.Check, base_priority: :high, category: :refactoring
+
+  @doc false
+  def run(source_file, params \\ []) do
+    issue_meta = IssueMeta.for(source_file, params)
+
+    namespaces =
+      params
+      |> Keyword.get(:namespaces, [])
+      |> Enum.flat_map(&Module.split/1)
+
+    source_file
+    |> Credo.Code.prewalk(&check_imports(&1, &2, namespaces, issue_meta), {[], []})
+    |> elem(0)
+  end
+
+  defp check_imports({:alias, _, [{:__aliases__, _, alias_parts}]} = ast, {issues, aliases}, _, _) do
+    alias_parts = alias_parts |> Module.concat() |> Module.split()
+    {ast, {issues, [alias_parts | aliases]}}
+  end
+
+  defp check_imports(
+         {:import, meta, [{:__aliases__, _, module}]} = ast,
+         {issues, aliases},
+         namespaces,
+         issue_meta
+       ) do
+    [namespace | _] = module |> Module.concat() |> Module.split()
+
+    namespace =
+      Enum.find_value(aliases, namespace, fn alias_parts ->
+        if List.last(alias_parts) == namespace do
+          hd(alias_parts)
+        end
+      end)
+
+    if namespace in namespaces do
+      {ast, {[issue_for(issue_meta, Keyword.get(meta, :line), namespace) | issues], aliases}}
+    else
+      {ast, {issues, aliases}}
+    end
+  end
+
+  defp check_imports(ast, issues, _, _) do
+    {ast, issues}
+  end
+
+  defp issue_for(issue_meta, line_no, trigger) do
+    format_issue(issue_meta,
+      message: "Import from configured namespaces found",
+      line_no: line_no,
+      trigger: trigger
+    )
+  end
+end
---
+++ test/avoid_imports_from_current_application_test.exs
@@ -0,0 +1,70 @@
+defmodule Nicene.AvoidImportsFromCurrentApplicationTest do
+  use Assertions.Case
+
+  alias Credo.{Issue, SourceFile}
+
+  alias Nicene.AvoidImportsFromCurrentApplication
+
+  test "finds issues" do
+    """
+    defmodule App.File do
+      alias AppWeb.Api
+
+      import Api.Helpers
+
+      import App.Helpers
+
+      def test() do
+        get_response()
+        handle_event()
+        :ok
+      end
+    end
+    """
+    |> SourceFile.parse("lib/app/file.ex")
+    |> AvoidImportsFromCurrentApplication.run([namespaces: [App, AppWeb]])
+    |> assert_issues([
+      %Issue{
+        category: :refactoring,
+        check: AvoidImportsFromCurrentApplication,
+        filename: "lib/app/file.ex",
+        line_no: 4,
+        message: "Import from configured namespaces found"
+      },
+      %Issue{
+        category: :refactoring,
+        check: AvoidImportsFromCurrentApplication,
+        filename: "lib/app/file.ex",
+        line_no: 6,
+        message: "Import from configured namespaces found"
+      }
+    ])
+  end
+
+  test "doesn't have false positives" do
+    """
+    defmodule App.File do
+      alias AppWeb.Api
+
+      import Api.Helpers
+
+      import App.Helpers
+
+      def test() do
+        get_response()
+        handle_event()
+        :ok
+      end
+    end
+    """
+    |> SourceFile.parse("lib/app/file.ex")
+    |> AvoidImportsFromCurrentApplication.run([namespaces: [Apple, AppleWeb]])
+    |> assert_issues([])
+  end
+
+  defp assert_issues(issues, expected) do
+    assert_lists_equal(issues, expected, fn issue, expected ->
+      assert_structs_equal(issue, expected, [:category, :check, :filename, :line_no, :message])
+    end)
+  end
+end

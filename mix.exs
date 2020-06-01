94596fe754d38b495e984d7185931b5b210ddf80
Ciarán Walsh <git@ciaranwal.sh>
2020-06-01 10:43:54 +0100

    Update assertions library

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Nicene.MixProject do
 
   defp deps() do
     [
-      {:assertions, "~> 0.15.0", only: [:test]},
+      {:assertions, "~> 0.17.0", only: [:test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.2.0"}
     ]
